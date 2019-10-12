150139a06c374a2a5aa32a31c213f85a867aa575
Felix Castro <felixcastro1993@gmail.com>
2019-10-12 17:45:39 -0500

    completo el ejercicio

--- calificaciones.java
+++ calificaciones.java
@@ -29,6 +29,9 @@ public class calificaciones {
        double [] califiEstu;
        int cantEstu;
        
+       System.out.println("CALIFICACIONES DE ESTUDIANTES");
+       System.out.println("------------------------------");
+       
        //solicitamos la cantidad de estudiante
        System.out.println("Ingrese la cantidad de estudiante");
        cantEstu = entrada.nextInt();
@@ -36,12 +39,16 @@ public class calificaciones {
        //vector
        califiEstu = new double[cantEstu];
        
+       System.out.println("------------------------------");
+       
        //ingreamos la notas
        System.out.println("Registro de notas de cada estudiante en orden");
        for (int i = 0; i < cantEstu; i++) {
            System.out.println((i+1)+".Ingrese la nota del estudiante ");
            califiEstu[i] = entrada.nextDouble();
        }
+       
+       System.out.println("------------------------------");
        
        //imprimimos las notas en el orden ingresado
        System.out.println("Notas de los estudiantes por orden ingresado");
@@ -66,8 +73,62 @@ public class calificaciones {
        //definimos promedio como constante
        double promNotas = 3;
        
+       System.out.println("------------------------------");
+       
        //imprimimos notas de los estudiantes que ganaron en forma decreciente
        System.out.println("Estudiantes que ganaron con promedio igual "
-               + "o superior a 3 en orden decreciente");
+               + "o superior a 3 en orden decreciente");       
+       for (int i = (cantEstu-1); i >= 0; i--) {
+           if (califiEstu[i]>=promNotas) {
+               System.out.println(califiEstu[i]);
+           }
+       }
+       
+       System.out.println("------------------------------");
+       
+       //imprimimos notas de los estudiantes que perdieron en forma creciente
+       System.out.println("Estudiantes que perdieron con promedio menor "
+               + "a 3 en orden creciente");       
+       for (int i = 0; i < cantEstu; i++) {
+           if (califiEstu[i]<promNotas) {
+               System.out.println(califiEstu[i]);
+           }
+       }
+       
+       System.out.println("------------------------------");
+       
+       // aqui buscamos la nota menor al promedio y se elimina
+       System.out.print("El promedio de las notas es :" + promNotas +"\n");
+       
+       System.out.println("------------------------------");
+       
+       int posicioneliminar = -1;
+       for (int i=0; i < califiEstu.length; i++) {
+           if(califiEstu[i] < promNotas){
+                  posicioneliminar = i;
+                  califiEstu[i] = 0;
+                  if(posicioneliminar >= 0){
+                       System.out.print("El dato en la posicion "
+                               + posicioneliminar +" se eliminó.\n");
+                   }else{
+                       System.out.print("No se encontro el dato. " +"\n");
+                   }
+               }
+        }       
+       
+       System.out.println("------------------------------");
+       
+        System.out.println("notas despues de eliminar");
+        for (int i = 0; i < cantEstu; i++) {
+            System.out.println(califiEstu[i]);
+        }
+        
+        System.out.println("------------------------------");
+        
+        //mostramos las notas despues que se elimino
+        System.out.println("notas despues de eliminar");
+        for (int i = 0; i < cantEstu; i++) {
+            System.out.println(califiEstu[i]);
+       }
    }
 }
